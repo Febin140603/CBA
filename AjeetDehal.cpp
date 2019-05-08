de8a7d0d308badbfe06abe4d906ab80846d62b11
AjeetDehal <44584976+AjeetDehal@users.noreply.github.com>
2019-05-08 16:57:29 +0530

    Create AjeetDehal.cpp

---
+++ AjeetDehal.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include <algorithm> 
+using namespace std; 
+int main() 
+{ 
+	int v[10] = { 9, 4, 7, 2, 5, 10, 11, 12, 1, 3 }; 
+
+	int* i1; 
+	i1 = std::min_element(v+0,v+10); 
+
+	cout << *i1 << "\n"; 
+	return 0; 
+}
